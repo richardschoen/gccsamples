274d31eb25ab04a7ec9d5c38c98904e039c6d289
Richard Schoen <richard@richardschoen.net>
2020-12-17 00:55:46 -0600

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+//https://www.tutorialspoint.com/compiling-a-cplusplus-program-with-gcc
+//gcc -o hellocpp hellocpp.cpp -lstdc++
+#include <iostream>
+using namespace std;
+main() {
+   cout << "Hello World. This is C++ program" << endl;
+}
