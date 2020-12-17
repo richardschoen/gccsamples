ebf60245b51dafccfe6ff5d38ac547b3fa284d98
Richard Schoen <richard@richardschoen.net>
2020-12-17 00:57:04 -0600

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,5 @@
 //https://www.tutorialspoint.com/compiling-a-cplusplus-program-with-gcc
-//gcc -o hellocpp hellocpp.cpp -lstdc++
+// Building this program: gcc -o hellocpp hellocpp.cpp -lstdc++
 #include <iostream>
 using namespace std;
 main() {
