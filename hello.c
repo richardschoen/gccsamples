75973cfc9174f6d0fda50ababf33097753b3cd26
Richard Schoen <richard@richardschoen.net>
2020-12-22 09:40:32 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,10 @@
+//---------------------------------------------------------------------------------------------
+// Program: hello.c
+// Purpose: Hello World
+// Parms: None
+// 
 // Building this program: gcc -o hello hello.c
+//---------------------------------------------------------------------------------------------
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
